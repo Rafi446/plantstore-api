34feb7f57adfd76bbe2f0e78e867623fbc81a8be
Rafi446 <rfdl11611@gmail.com>
2026-09-06 10:45:42 +0700

    feat: add category api

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable
     
     public function orders()
     {
-        return $this->hasOne(Order::class);
+        return $this->hasMany(Order::class);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -11,5 +12,7 @@ Route::middleware('auth:sanctum')->group(function (){
     Route::get('/user', [AuthController::class, 'profil']);
     
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('category', CategoryController::class);
 
 });
